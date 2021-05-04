b0e269212acb6d3ab7ce6f929bced450c43465ed
mathieu gonzalez <mathieu.gonzalez@ynov.com>
2021-05-04 10:05:48 +0200

    init git

---
+++ source/API/matlasAPI/src/main/java/com/matlas/matlasAPI/MatlasApiApplication.java
@@ -0,0 +1,13 @@
+package com.matlas.matlasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatlasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatlasApiApplication.class, args);
+	}
+
+}
