b9ee4a925792f61918b2efbfac64f05dcdbe83fe
mathieu gonzalez <mathieu.gonzalez@ynov.com>
2021-05-13 17:29:06 +0200

    add prototype

---
+++ source/prototype/prototype/src/main/java/com/matlas/prototype/Main.java
@@ -0,0 +1,12 @@
+package com.matlas.prototype;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello World !");
+
+	}
+
+}
