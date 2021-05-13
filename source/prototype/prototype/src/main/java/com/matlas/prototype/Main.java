22c092b2c842d68dcf5057687c3ad55d71a3a248
mathieu gonzalez <mathieu.gonzalez@ynov.com>
2021-05-13 18:52:11 +0200

    génération de pdf basic

--- source/prototype/prototype/src/main/java/com/matlas/prototype/Main.java
+++ source/prototype/prototype/src/main/java/com/matlas/prototype/Main.java
@@ -1,12 +1,32 @@
 package com.matlas.prototype;
 
+import java.io.File;
+import java.lang.module.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+import static org.asciidoctor.Asciidoctor.Factory.create;
+import static org.asciidoctor.OptionsBuilder.options;
+import org.asciidoctor.Asciidoctor;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("hello World !");
+		Asciidoctor asciidoctor = create();
+		
+		
+		Map<String, Object> options = options()
+				  .inPlace(true)
+				  .backend("pdf")
+				  .asMap();
+
+				String outfile = asciidoctor.convertFile(new File("src\\main\\resources\\matlasPla.adoc"), options);
+		
 
 	}
+	
+	
 
 }
